e34a776278efec45d43a99ad41788af0ff1d74c9
miguellz67 <luizmiguel67h@outlook.com>
2020-09-13 20:35:01 -0300

    Página principal base

--- src/app/Main/main.module.ts
+++ src/app/Main/main.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { MainComponent } from './main/main.component';
 import { MainRouterModule } from './main-routing.module';
 import { MainAppComponent } from './main-app';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 @NgModule({
-    declarations: [ MainComponent, MainAppComponent ],
-    imports: [ MainRouterModule ],
+    declarations: [
+        MainComponent,
+        MainAppComponent
+    ],
+    imports: [
+        MainRouterModule,
+        TooltipModule.forRoot()
+    ],
     exports: [ MainComponent ]
 })
 
