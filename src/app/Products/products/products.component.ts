ce40e12fa2d29240cfc5d599852dd83abda5c7dd
miguellz67 <luizmiguel67h@outlook.com>
2020-09-22 19:55:53 -0300

    Modulo e pagina de catalago iniciais

--- src/app/Products/products/products.component.ts
+++ src/app/Products/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/services/Product/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  faSearch = faSearch;
+  colums = 5;
+  product: Product;
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe((pr: Product[]) => {
+      this.product = pr[0];
+    });
   }
 
 }
