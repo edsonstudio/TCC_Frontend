eecb95c2632f1984310b0f1795b31192d39069a4
miguellz67 <luizmiguel67h@outlook.com>
2020-09-24 14:29:41 -0300

    Injecao de servico e rotas

--- src/app/Account/account-routing.module.ts
+++ src/app/Account/account-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountAppComponent } from './account-app';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const accountRouteConfig: Routes = [
     { path: '', component: AccountAppComponent,
         children: [
-            { path: 'cadastrar' },
-            { path: 'entrar' }
+            { path: 'cadastrar', component: RegisterComponent},
+            { path: 'entrar', component: LoginComponent }
         ] }
 ];
 
--- src/app/Account/account.module.ts
+++ src/app/Account/account.module.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AccountAppComponent } from './account-app';
+
+import { SharedModule } from '../components/shared.module';
 import { AccountRoutingModule } from './account-routing.module';
+import { AccountService } from '../services/User/user.service';
+
+import { AccountAppComponent } from './account-app';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        AccountRoutingModule
+        AccountRoutingModule,
+        SharedModule
     ],
     declarations: [
-        AccountAppComponent
+        AccountAppComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AccountService
     ]
 })
 
