b7e8cc4c70964facfdc535bb99db22708c8f512d
miguellz67 <luizmiguel67h@outlook.com>
2020-09-28 10:42:07 -0300

    Resolve de produto

--- src/app/Products/product-routing.module.ts
+++ src/app/Products/product-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductResolve } from '../services/Product/product.resolve';
 import { ProductAppComponent } from './product-app';
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products/products.component';
@@ -8,7 +9,7 @@ const productRouterConfig: Routes = [
     { path: '', component: ProductAppComponent,
     children: [
         { path: '', component: ProductsComponent },
-        { path: ':id', component: ProductComponent }
+        { path: ':id', component: ProductComponent, resolve: { product: ProductResolve } }
     ]}
 ];
 
