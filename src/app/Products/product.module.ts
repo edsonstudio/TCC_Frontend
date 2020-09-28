01b73f7ce2906ccec2e75574f1fc10113ed1755f
miguellz67 <luizmiguel67h@outlook.com>
2020-09-28 10:43:48 -0300

    Imports: NgxPagination Ng2Search Forms

--- src/app/Products/product.module.ts
+++ src/app/Products/product.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductRouterModule } from './product-routing.module';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { SharedModule } from '../components/shared.module';
+
+import { ProductResolve } from '../services/Product/product.resolve';
+
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductAppComponent } from './product-app';
-import { ProductRouterModule } from './product-routing.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CardComponent } from '../components/card/card.component';
-import { SharedModule } from '../components/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductRouterModule,
     FontAwesomeModule,
-    SharedModule
+    SharedModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   declarations: [
     ProductAppComponent,
     ProductComponent,
     ProductsComponent
+  ],
+  providers: [
+    ProductResolve
   ]
 })
 export class ProductModule { }
