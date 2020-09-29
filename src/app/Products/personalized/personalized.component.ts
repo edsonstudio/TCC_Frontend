4eb222f3862855aad7305028231a2b04f8bc79b2
miguellz67 <luizmiguel67h@outlook.com>
2020-09-29 19:50:21 -0300

    Component de montagem base

---
+++ src/app/Products/personalized/personalized.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personalized',
+  templateUrl: './personalized.component.html',
+  styleUrls: ['./personalized.component.scss']
+})
+export class PersonalizedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
