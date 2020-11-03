31406f5b2e201f79d6bd3593796ec9407ecebb96
miguellz67 <luizmiguel67h@outlook.com>
2020-11-03 20:38:15 -0300

    Permissionamento para acesso

--- src/app/Admin/admin-routing.module.ts
+++ src/app/Admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAccessGuard } from '../services/User/user-access.guard';
 import { ManagementComponent } from './management/management.component';
 
 const adminRouteConfig: Routes = [
-    { path: '', component: ManagementComponent }
+    { path: '', component: ManagementComponent,
+        data: [{claim: {name: 'Products', value: 'Adicionar, Excluir, Editar'}}], canActivate: [UserAccessGuard] }
 ];
 
 @NgModule({
