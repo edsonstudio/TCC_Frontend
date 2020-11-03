fd215ea1b2df54035b87a55b37250c626d7fb298
miguellz67 <luizmiguel67h@outlook.com>
2020-11-03 20:40:59 -0300

    Base

--- src/app/Admin/admin.module.ts
+++ src/app/Admin/admin.module.ts
@@ -1,4 +1,24 @@
 import { NgModule } from '@angular/core';
+import { AdminRoutingModule } from './admin-routing.module';
+import { ManagementComponent } from './management/management.component';
+import { SidebarModule } from 'ng-sidebar';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AdmProductsComponent } from '../Admin/products/admproducts/admproducts.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AdmUsersComponent } from './users/admusers/admusers.component';
 
-@NgModule({})
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        AdminRoutingModule,
+        SidebarModule,
+        TabsModule.forRoot()
+    ],
+    declarations: [
+        ManagementComponent,
+        AdmProductsComponent,
+        AdmUsersComponent
+    ]
+})
 export class AdminModule{}
