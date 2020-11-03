4fbda33aeb1472a18f57725247361ce60a6711a1
miguellz67 <luizmiguel67h@outlook.com>
2020-11-02 21:58:39 -0300

    Mascara de data do cartao

---
+++ src/app/components/input-mask.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[formControlName][appInputMask]',
+})
+export class InputMaskDirective {
+
+  @HostListener('input', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+
+    const trimmed = input.value.replace(/\s+/g, '').slice(0, input.value.indexOf('/') === -1 ? 4 : 5);
+    if (trimmed.length > 3) {
+      return (input.value = `${trimmed.slice(0, 2)}/${trimmed.slice(trimmed.indexOf('/') === -1 ? 2 : 3)}`);
+    }
+  }
+
+}
