dd3c2795648de026d502473b1532ef75facecd8c
miguellz67 <luizmiguel67h@outlook.com>
2020-11-02 22:04:31 -0300

    Servico de pedidos

---
+++ src/app/services/Cart_Order/order.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+
+export class OrderService extends BaseService {
+    constructor(private http: HttpClient){
+        super();
+    }
+
+    applyVoucher(codeV){
+        return this.http.post(`${this.UrlShopping}/carrinho/aplicar-voucher`, JSON.stringify(codeV), this.GetJsonAuthHeader())
+        .pipe(
+            map(this.extractData),
+            catchError(this.serviceError)
+        );
+    }
+
+    sentOrder(order){
+        return this.http.post(`${this.UrlShopping}/pedido`, JSON.stringify(order), this.GetJsonAuthHeader());
+    }
+
+    CEP(cep: string): Observable<AdResponse> {
+
+        console.log(cep);
+        cep = cep.replace(/\D/g, '');
+        if (cep !== '') {
+
+          const validacep = /^[0-9]{8}$/;
+          // Valida o formato do CEP.
+          if (validacep.test(cep)) {
+            return this.http.get(`//viacep.com.br/ws/${cep}/json`);
+          }
+        }
+        return of({});
+      }
+}
+
+interface AdResponse {
+    bairro?: string;
+    cep?: string;
+    complemento?: string;
+    ddd?: string;
+    gia?: string;
+    ibge?: string;
+    localidade?: string;
+    logradouro?: string;
+    siafi?: string;
+    uf?: string;
+}
