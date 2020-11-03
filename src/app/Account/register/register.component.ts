dbdd218acda8f1bcb7ad01b46ddaf2a0ed40a80a
miguellz67 <luizmiguel67h@outlook.com>
2020-11-02 21:53:35 -0300

    Erros de resposta do cadastro

--- src/app/Account/register/register.component.ts
+++ src/app/Account/register/register.component.ts
@@ -12,7 +12,7 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit, OnChanges {
+export class RegisterComponent implements OnInit {
 
   user: User;
   errorsResponse: string[];
@@ -25,22 +25,63 @@ export class RegisterComponent implements OnInit, OnChanges {
     private router: Router
     ) { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-
-  }
 
   ngOnInit() {
+    this.errorsResponse = [];
   }
 
 
   register(event: User){
     this.user = event;
+    console.log(event);
     this.accountService.registerUser(this.user).subscribe(
       success => {
         this.processSuccess(success);
       },
       fail => {
-        this.errorsResponse = fail.error.errors;
+        this.errorsResponse = [];
+        if (fail.error.errors.Name){
+          const nameError = fail.error.errors.Name;
+          nameError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.Cpf){
+          const cpfError = fail.error.errors.Cpf;
+          cpfError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.Phone){
+          const phoneError = fail.error.errors.Phone;
+          phoneError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.Email){
+          const emailError = fail.error.errors.Email;
+          emailError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.Password){
+          const pwdError = fail.error.errors.Password;
+          pwdError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.ConfirmPassword){
+          const cpwdError = fail.error.erros.ConfirmPassword;
+          cpwdError.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
+        if (fail.error.errors.Mensagens){
+          const errors = fail.error.errors.Mensagens;
+          errors.forEach(element => {
+            this.errorsResponse.push(element);
+          });
+        }
         this.cleanForm = false;
       }
     );
@@ -48,14 +89,16 @@ export class RegisterComponent implements OnInit, OnChanges {
 
   processSuccess(response){
         this.cleanForm = true;
+        this.changesNotSaved = false;
         this.errorsResponse = [];
         console.clear();
 
         this.accountService.LocalStorage.saveUserLocalData(response);
 
-        const toastr = this.toastr.success('Cadastro feito com sucesso!', 'Sucesso', {
+        const toastr = this.toastr.success('Cadastro feito com sucesso!', 'Massa', {
           progressBar: true,
           easing: 'ease',
+          timeOut: 2000
         });
         if (toastr){
           toastr.onHidden.subscribe(() => {
