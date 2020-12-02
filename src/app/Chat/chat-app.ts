7f2bf7ec52e67d86154a688c43074e49b43d1ebd
miguellz67 <luizmiguel67h@outlook.com>
2020-12-02 00:05:05 -0300

    Navbar

--- src/app/Chat/chat-app.ts
+++ src/app/Chat/chat-app.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'chat-app',
-    template: '<router-outlet></router-outlet>'
+    template: '<router-outlet></router-outlet><app-navbar></app-navbar>'
 })
 
 export class ChatAppComponent{}
