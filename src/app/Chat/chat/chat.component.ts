c0a88e929389336c6be2238f829d788e586768eb
miguellz67 <luizmiguel67h@outlook.com>
2020-12-02 00:05:46 -0300

    Chat com bug

--- src/app/Chat/chat/chat.component.ts
+++ src/app/Chat/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone, OnInit } from '@angular/core';
-import { Message } from 'src/app/models/Message';
-import { User } from 'src/app/models/User';
+import { MenuItem } from 'primeng/api';
+import { Message, Thread } from 'src/app/models/Message';
+import { Oponent, Profile, User } from 'src/app/models/User';
 import { ChatService } from 'src/app/services/Chat/chat.service';
 import { environment } from 'src/environments/environment';
 
@@ -12,20 +13,37 @@ import { environment } from 'src/environments/environment';
 export class ChatComponent implements OnInit {
 
   urlImg = environment.images;
-  txtMessage: string = '';
+  filter = 'a';
+  txtMessage = '';
   uniqueID: string;
+  home = {icon: 'pi pi-home', routerLink: '/Inicio'};
+  bread: MenuItem[] = [
+    {label: 'Suporte'},
+    {label: 'Chat'},
+  ];
   messages = new Array<Message>();
+  profile = new Profile();
   message = new Message();
   users = new Array<User>();
+  threads = new Array<Thread>();
+  currentThreadId: string;
+  listForC = new Array<any>();
+  currentOponentVM: Oponent;
   constructor(
     private chatService: ChatService,
-    private _ngZone: NgZone
+    private _ngZone: NgZone,
+
   ) {
+    this.chatService.createConnection();
+    this.chatService.registerOnServerEvents();
+    this.chatService.startConnection();
     this.subscribeToEvents();
-    this.uniqueID = this.chatService.LocalStorage.getUserToken();
   }
 
-  con(user){console.log('deu certo', user)}
+  con(user){
+    console.log('deu certo', user);
+    this.loadThread(user);
+  }
 
   ngOnInit() {
     console.log('this');
@@ -36,33 +54,98 @@ export class ChatComponent implements OnInit {
           const urlImg = this.chatService.getDefaultImageUrl(user.username);
           user.avatarFileName = urlImg;
         }
+        else {
+          user.avatarFileName = `${this.urlImg}/${user.avatarFileName}`;
+        }
       });
-      console.log(this.users);
     }, error => console.log(error));
+    this.chatService.getThreads().subscribe(threads => {
+      this.threads = threads;
+      this.threads.forEach(tr => {
+        if (tr.oponentVM.avatarFileName === null){
+          const url = this.chatService.getDefaultImageUrl(tr.oponentVM.username);
+          tr.oponentVM.avatarFileName = url;
+        }
+        else {
+          tr.oponentVM.avatarFileName = `${this.urlImg}/${tr.oponentVM.avatarFileName}`;
+        }
+      });
+      this.listForC = threads;
+    });
+    this.uniqueID = this.chatService.LocalStorage.getUserToken();
+    this.chatService.getProfile(this.uniqueID).subscribe(prof => this.profile = prof);
   }
 
+  loadThread(event: Thread){
+    console.log('entrou');
+    this.messages = [];
+    this.currentOponentVM = event.oponentVM;
+    this.currentThreadId = event.id;
+    this.chatService.getMessages(event.id, this.uniqueID).subscribe(result => {
+      const messages = Object.entries(result)[0][1];
+      messages.forEach(message => {
+        if (message.senderId !== this.profile.id){
+          message.type = 'received';
+          this.messages.push(message);
+        }
+        else {
+          message.type = 'sent';
+          this.messages.push(message);
+        }
+      });
+      console.log(this.messages);
+    });
+  }
+
+  createThread(event: Oponent){
+    const result = this.threads.find(thread => thread.oponentVM.id === event.id);
+    if (result){
+      this.loadThread(result);
+    }
+    else {
+      const newthr = new Thread();
+      newthr.oponentVM = event;
+      newthr.owner = this.profile.id;
+      this.chatService.createThread(newthr).subscribe(thr => {
+        this.currentThreadId = thr.id;
+        this.currentOponentVM = event;
+      });
+    }
+  }
 
   sendMessage(): void {
     if (this.txtMessage) {
       this.message = new Message();
-      this.message.senderId = this.uniqueID;
+      this.message.senderId = this.profile.id;
       this.message.text = this.txtMessage;
-      this.message.time = new Date();
-      const msg = this.message;
-      msg.type = 'sent';
-      this.messages.push(msg);
-      this.chatService.sendMessageToApi(this.message).subscribe(
-        success => console.log('Mensagem enviada'),
-        error => console.log(error)
+      this.message.time = new Date().toISOString();
+      this.message.date = new Date().toISOString();
+      this.message.threadId = this.currentThreadId;
+      this.message.username = this.profile.userName;
+      this.message.type = 'sent';
+
+      const msgEnv: Message = {
+        text: this.message.text,
+        senderId: this.message.senderId,
+        time: this.message.time,
+        date: this.message.date,
+        threadId: this.message.threadId,
+        username: this.message.username
+      };
+      this.messages.push(this.message);
+      console.log(this.message);
+      this.chatService.sendMessageToApi(msgEnv).subscribe(
+         success => console.log('Mensagem enviada'),
+         error => console.log(error)
       );
       this.txtMessage = '';
     }
   }
-  private subscribeToEvents(): void {
 
+  subscribeToEvents(): void {
     this.chatService.messageReceived.subscribe((message: Message) => {
       this._ngZone.run(() => {
-        if (message.senderId !== this.uniqueID) {
+        if (message.senderId !== this.profile.id) {
           message.type = 'received';
           this.messages.push(message);
         }
