9b6afcbcbbe512654a817d56f6b03307448159d2
miguellz67 <luizmiguel67h@outlook.com>
2020-12-02 00:13:13 -0300

    Novo layout com bug no personalized

--- src/app/Products/product/product.component.ts
+++ src/app/Products/product/product.component.ts
@@ -1,30 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Subscription } from 'rxjs';
+import { CartItem } from 'src/app/models/CartItem';
+import { CartService } from 'src/app/services/Cart_Order/cart.service';
 import { CommumMethods } from 'src/app/services/commum-methods';
 import { environment } from 'src/environments/environment';
+import { Store } from '../cart.store';
 import { Product } from './../../models/Product';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent extends CommumMethods implements OnInit {
+export class ProductComponent extends CommumMethods implements OnInit, OnDestroy {
 
   images = environment.images;
   product: Product;
+  val: number;
+  subscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private spinner: NgxSpinnerService, private routeS: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private routeS: Router,
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig,
+    private store: Store,
+    private cart: CartService) {
     super();
     this.LoadingSpinner(this.routeS, this.spinner);
   }
 
   ngOnInit() {
-    this.product = this.route.snapshot.data.product;
-    console.log(this.product);
+    this.product = this.config.data.product;
+    this.subscription = this.cart.getCart$.subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
+   buyItem(){
+    this.cart.getCart$.pipe(map(cart => cart.items)).toPromise().then(cartItems => {
+      const prod = cartItems.find(cartItem => cartItem.productId === this.product.id);
+      if (prod) {
+        this.ref.close();
+        this.routeS.navigate(['/Inicio/produtos/carrinho']);
+      }
+      else {
+        const caartIt: CartItem = {
+          amount: 1,
+          image: this.product.image,
+          name: this.product.name,
+          price: this.product.price,
+          productId: this.product.id,
+          categoryId: this.product.categoryId
+        };
+        this.cart.postCartItem(caartIt).subscribe(
+          success => {
+            this.ref.close();
+            this.routeS.navigate(['/Inicio/produtos/carrinho']);
+          },
+          error => {console.log(error); }
+        );
+      }
+    });
+  }
 
 }
--- src/app/Products/products/products.component.ts
+++ src/app/Products/products/products.component.ts
@@ -1,37 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MenuItem, MessageService, PrimeNGConfig, SelectItem } from 'primeng/api';
+import { CartItem } from 'src/app/models/CartItem';
 import { Category } from 'src/app/models/Category';
 import { Product } from 'src/app/models/Product';
+import { CartService } from 'src/app/services/Cart_Order/cart.service';
 import { CategoryService } from 'src/app/services/Category/category.service';
 import { ProductService } from 'src/app/services/Product/product.service';
 import { environment } from 'src/environments/environment';
+import { Store } from '../cart.store';
+import { Subscription } from 'rxjs';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ProductComponent } from '../product/product.component';
+import { map } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CommumMethods } from 'src/app/services/commum-methods';
 
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
-  host: { "(window:resize)":"onWindowResize($event)" }
+  host: { "(window:resize)": "onWindowResize($event)" }
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent extends CommumMethods implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private spinner: NgxSpinnerService,
     private categoryService: CategoryService,
-    private router: Router) { }
+    private cartService: CartService,
+    private router: Router,
+    private primengConfig: PrimeNGConfig,
+    private store: Store,
+    private cart: CartService,
+    private messageService: MessageService,
+    private dialogService: DialogService
+    ) {
+      super();
+      this.LoadingSpinner(this.router, this.spinner);
+    }
 
+
+  val = 0;
+  display = true;
+  warningLogin = false;
+  addItem = false;
   prlength: number;
   win = window;
   actualPag = 1;
   search: string;
+  home = {icon: 'pi pi-home', routerLink: '/Inicio'};
   images = environment.images;
   faSearch = faSearch;
+  displayy = false;
   colums: number;
   products: Product[];
   categories: Category[];
+  bread: MenuItem[] = [
+    {label: 'Produtos'},
+    {label: 'Todos'},
+  ];
+  sortOptions: SelectItem[];
+  subscription: Subscription;
   width: number = window.innerWidth;
   height: number = window.innerHeight;
+  sortOrder: number;
+  sortField: string;
+  cartItems: number;
+
   _pag = 4;
   get pag(){
     return this._pag;
@@ -41,14 +79,19 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.primengConfig.ripple = true;
     this.getAll();
     this.getCategories();
+    this.sortOptions = [
+      {label: 'Preço Crescente', value: 'price'},
+      {label: 'Preço Decrescente', value: '!price'}
+  ];
+    this.subscription = this.cart.getCart$.subscribe();
   }
 
   onWindowResize(event) {
     this.width = event.target.innerWidth;
     this.height = event.target.innerHeight;
-    console.log(this.width);
 }
 
   filterProducts(category: Category, brand: string){
@@ -57,6 +100,10 @@ export class ProductsComponent implements OnInit {
 
   filterPrCategory(category: Category){
     this.products = category.products;
+  }
+
+  xx(){
+    this.displayy = true;
   }
 
   getCategories(){
@@ -72,22 +119,93 @@ export class ProductsComponent implements OnInit {
         this.colums = (pr.length + 1) / 2;
       }else {this.colums = pr.length / 2; }
       this.prlength = pr.length;
-      console.log(this.products);
     });
   }
 
   Brands(category: Category){
-    const brands: Product[] = [];
+    let strings = new Array<string>();
+    category.products.forEach(pr => strings.push(pr.brand));
 
-    category.products.forEach(vl => {
-      if (category.products.filter(pr => pr.brand === vl.brand).length <= 1) { brands.push(vl); }
-    });
-
-    return brands;
+    return strings.filter((brand, index) => strings.indexOf(brand) === index);
   }
 
   Navigate(product: Product){
    this.router.navigate([`Inicio/produtos/todos/${product.id}`]);
+  }
+
+  onWheel(event: WheelEvent): void {
+    (event.target as Element).parentElement.scrollLeft += event.deltaY;
+    event.preventDefault();
+  }
+
+  show(productt: Product){
+    const ref = this.dialogService.open(ProductComponent, {
+      data: {
+        product: productt
+      },
+      width: '70%'
+    });
+  }
+
+  onSortChange(event) {
+    const value = event.value;
+
+    if (value.indexOf('!') === 0) {
+        this.sortOrder = -1;
+        this.sortField = value.substring(1, value.length);
+    }
+    else {
+        this.sortOrder = 1;
+        this.sortField = value;
+    }
+  }
+
+  navigateToLogin(){
+    this.messageService.clear();
+    this.router.navigate(['Inicio/conta/entrar']);
+  }
+
+  showCustom(product?: Product) {
+    this.clearMessages();
+    this.messageService.add({severity: 'custom', summary: 'Show!', detail: `${product.name} adicionado ao carrinho`, icon: 'pi-shopping-cart'});
+  }
+
+  showConfirm() {
+    this.clearMessages();
+    this.messageService.add({key: 'c', sticky: true, severity: 'warn', summary: 'Necessário Login', detail: 'Fazer Login ?'});
+  }
+
+  clearMessages(){
+    this.messageService.clear();
+  }
+
+  buyItem(product: Product){
+    if (!this.productService.LocalStorage.getUser()){
+      this.showConfirm();
+    }
+    else {
+      this.cart.getCart$.toPromise().then(cartItems => {
+        const prod = cartItems.items.find(cartItem => cartItem.productId === product.id);
+        if (prod) {
+          this.showCustom(product);
+          this.subscription.unsubscribe();
+        }
+        else {
+          const caartIt: CartItem = {
+            amount: 1,
+            image: product.image,
+            name: product.name,
+            price: product.price,
+            productId: product.id,
+            categoryId: product.categoryId
+          };
+          this.cart.postCartItem(caartIt).toPromise().then(() => {
+              this.showCustom(product);
+              this.subscription.unsubscribe();
+          });
+        }
+      });
+    }
   }
 
 }
