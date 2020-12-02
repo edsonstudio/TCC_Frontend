ceda13f8aa114e684079045fb09cbde71934a4e7
miguellz67 <luizmiguel67h@outlook.com>
2020-12-02 00:05:20 -0300

    Imports primeng

--- src/app/Chat/chat.module.ts
+++ src/app/Chat/chat.module.ts
@@ -6,13 +6,25 @@ import { ChatAppComponent } from './chat-app';
 import { ChatService } from '../services/Chat/chat.service';
 import {AccordionModule} from 'primeng/accordion';
 import {OrderListModule} from 'primeng/orderlist';
+import {FieldsetModule} from 'primeng/fieldset';
+import {ListboxModule} from 'primeng/listbox';
+import { SharedModule } from '../components/shared.module';
+import {ButtonModule} from 'primeng/button';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   imports: [
     CommonModule,
     ChatRoutingModule,
     AccordionModule,
-    OrderListModule
+    OrderListModule,
+    FieldsetModule,
+    ListboxModule,
+    SharedModule,
+    ButtonModule,
+    BreadcrumbModule,
+    InputTextModule
   ],
   declarations: [
     ChatComponent,
