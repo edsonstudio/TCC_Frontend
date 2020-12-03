72c370b55cb881db4561005d68a1797fa59eace3
miguellz67 <luizmiguel67h@outlook.com>
2020-12-03 15:50:11 -0300

    Verificando login

--- src/app/Products/product-routing.module.ts
+++ src/app/Products/product-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActionsGuard } from '../services/actions.guard';
 import { PersonalizedComponent } from './personalized/personalized.component';
 import { ProductAppComponent } from './product-app';
 import { ProductsComponent } from './products/products.component';
@@ -8,8 +9,8 @@ const productRouterConfig: Routes = [
     { path: '', component: ProductAppComponent,
     children: [
         { path: 'todos', component: ProductsComponent },
-        { path: 'meu-setup', component: PersonalizedComponent },
-        { path: 'pedido', loadChildren: () => import('../Cart/cart.module').then(m => m.CartModule)},
+        { path: 'meu-setup', component: PersonalizedComponent, canActivate: [ActionsGuard] },
+        { path: 'pedido', canActivate: [ActionsGuard], loadChildren: () => import('../Cart/cart.module').then(m => m.CartModule)},
         { path: '**', redirectTo: ''}
     ]}
 ];
