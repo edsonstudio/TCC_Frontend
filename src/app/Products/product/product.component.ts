4f3fc3db96c031ba02303bd25820fcdbfebd34a4
miguellz67 <luizmiguel67h@outlook.com>
2020-12-03 15:50:50 -0300

    Spinner e toast para login

--- src/app/Products/product/product.component.ts
+++ src/app/Products/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
@@ -10,65 +10,93 @@ import { environment } from 'src/environments/environment';
 import { Store } from '../cart.store';
 import { Product } from './../../models/Product';
 import { map } from 'rxjs/operators';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent extends CommumMethods implements OnInit, OnDestroy {
+export class ProductComponent extends CommumMethods implements OnInit, OnDestroy, AfterViewInit {
 
   images = environment.images;
   product: Product;
   val: number;
   subscription: Subscription;
+  logged = false;
 
   constructor(
     private route: ActivatedRoute,
     private spinner: NgxSpinnerService,
+    private messageService: MessageService,
     private routeS: Router,
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig,
     private store: Store,
+    private router: Router,
     private cart: CartService) {
     super();
-    this.LoadingSpinner(this.routeS, this.spinner);
   }
 
   ngOnInit() {
+    this.spinner.show();
+    if (this.cart.LocalStorage.getUser()){
+      this.logged = true;
+    }
     this.product = this.config.data.product;
     this.subscription = this.cart.getCart$.subscribe();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1500);
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
+  clearMessages(){
+    this.messageService.clear('c');
+  }
+
+  showConfirm() {
+    this.clearMessages();
+    this.messageService.add({key: 'c', sticky: true, severity: 'warn', summary: 'Necessário Login', detail: 'Fazer Login ?'});
+  }
+
    buyItem(){
-    this.cart.getCart$.pipe(map(cart => cart.items)).toPromise().then(cartItems => {
-      const prod = cartItems.find(cartItem => cartItem.productId === this.product.id);
-      if (prod) {
-        this.ref.close();
-        this.routeS.navigate(['/Inicio/produtos/carrinho']);
-      }
-      else {
-        const caartIt: CartItem = {
-          amount: 1,
-          image: this.product.image,
-          name: this.product.name,
-          price: this.product.price,
-          productId: this.product.id,
-          categoryId: this.product.categoryId
-        };
-        this.cart.postCartItem(caartIt).subscribe(
-          success => {
-            this.ref.close();
-            this.routeS.navigate(['/Inicio/produtos/carrinho']);
-          },
-          error => {console.log(error); }
-        );
-      }
-    });
+      this.cart.getCart$.pipe(map(cart => cart.items)).toPromise().then(cartItems => {
+        const prod = cartItems.find(cartItem => cartItem.productId === this.product.id);
+        if (prod) {
+          this.ref.close();
+          this.routeS.navigate(['/Inicio/produtos/carrinho']);
+        }
+        else {
+          const caartIt: CartItem = {
+            amount: 1,
+            image: this.product.image,
+            name: this.product.name,
+            price: this.product.price,
+            productId: this.product.id,
+            categoryId: this.product.categoryId
+          };
+          this.cart.postCartItem(caartIt).subscribe(
+            success => {
+              this.ref.close();
+              this.routeS.navigate(['/Inicio/produtos/carrinho']);
+            },
+            error => {console.log(error); }
+          );
+        }
+      });
+    
+  }
+
+  navigateToLogin(){
+    this.messageService.clear();
+    this.router.navigate(['Inicio/conta/entrar']);
   }
 
 }
