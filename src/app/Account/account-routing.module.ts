874880057ec5ce169e65de623ca2d109bfab70ff
miguellz67 <luizmiguel67h@outlook.com>
2020-12-07 10:16:34 -0300

    Rota de pedidos e guarda de login

--- src/app/Account/account-routing.module.ts
+++ src/app/Account/account-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActionsGuard } from '../services/actions.guard';
 import { GuardAccount } from '../services/User/user.guard';
 import { AccountAppComponent } from './account-app';
 import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
 import { RegisterComponent } from './register/register.component';
 
 const accountRouteConfig: Routes = [
     { path: '', component: AccountAppComponent,
         children: [
             { path: 'cadastrar', component: RegisterComponent, canActivate: [GuardAccount], canDeactivate: [GuardAccount]},
-            { path: 'entrar', component: LoginComponent, canActivate: [GuardAccount] }
+            { path: 'entrar', component: LoginComponent, canActivate: [GuardAccount] },
+            { path: 'pedidos', component: OrdersComponent, canActivate: [ActionsGuard] }
         ] }
 ];
 
