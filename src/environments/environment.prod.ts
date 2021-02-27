aaa41212493418c37cf138bf227dcbf6d1feda4c
Edson <edsont8@hotmail.com>
2021-02-27 13:57:49 -0300

    variaveis de ambiente de producao com url do heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,10 @@
 export const environment = {
-  production: true,
-  apiUrlV1: 'https://localhost:5305/api/v1',
-  apiUrlV2: 'https://localhost:5305/api/v2',
-  apiChatUrl: 'https://localhost:5101/api/v2',
-  apiAuthUrlV2: 'https://localhost:5101/api/v2/identity',
+  production: false,
+  apiUrlV1: 'https://heroku-products.herokuapp.com/api/v1',
+  apiUrlV2: 'https://heroku-products.herokuapp.com/api/v2',
+  apiChatUrl: 'https://heroku-auth-chat.herokuapp.com/api/v2',
+  apiAuthUrlV2: 'https://heroku-auth-chat.herokuapp.com/api/v2/identity',
   apiShoppingUrlV1: 'https://localhost:5501/api/v1/compras',
   apiClientUrl: 'https://localhost:5401/api/v1/client/address',
-  images: 'https://localhost:5305'
+  images: 'https://heroku-products.herokuapp.com/'
 };
-
