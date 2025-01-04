404aa7cf4286cc45e3a5cf94b8a12a537354f62b
edsonstudio <edson.costa.developer@gmail.com>
2025-01-04 18:40:23 -0300

    fix: update clients api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   apiChatUrl: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2',
   apiAuthUrlV2: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2/identity',
   apiShoppingUrlV1: 'https://localhost:5501/api/v1/compras',
-  apiClientUrl: 'https://localhost:5401/api/v1/client/address',
+  apiClientUrl: 'https://dragonstore-client-g4bydzetb8h4b7dt.brazilsouth-01.azurewebsites.net/api/v1/client/address',
   images: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/'
 };
