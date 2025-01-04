d51b67547d419d51132f912c776ea020dfafb430
edsonstudio <edson.costa.developer@gmail.com>
2025-01-04 17:38:47 -0300

    fix: update apis urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrlV1: 'https://heroku-products.herokuapp.com/api/v1',
-  apiUrlV2: 'https://heroku-products.herokuapp.com/api/v2',
-  apiChatUrl: 'https://heroku-auth-chat.herokuapp.com/api/v2',
-  apiAuthUrlV2: 'https://heroku-auth-chat.herokuapp.com/api/v2/identity',
+  apiUrlV1: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/api/v1',
+  apiUrlV2: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/api/v2',
+  apiChatUrl: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2',
+  apiAuthUrlV2: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2/identity',
   apiShoppingUrlV1: 'https://localhost:5501/api/v1/compras',
   apiClientUrl: 'https://localhost:5401/api/v1/client/address',
-  images: 'https://heroku-products.herokuapp.com/'
+  images: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/'
 };
