e7e3a1c10289b8e53fe9c5b6e716ee40918186e2
edsonstudio <edson.costa.developer@gmail.com>
2025-01-05 19:09:26 -0300

    fix: added bff url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrlV2: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/api/v2',
   apiChatUrl: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2',
   apiAuthUrlV2: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2/identity',
-  apiShoppingUrlV1: 'https://localhost:5501/api/v1/compras',
+  apiShoppingUrlV1: 'https://dragonstore-bff-cjcne7ahgwb3fjg6.brazilsouth-01.azurewebsites.net//api/v1/compras',
   apiClientUrl: 'https://dragonstore-client-g4bydzetb8h4b7dt.brazilsouth-01.azurewebsites.net/api/v1/client/address',
   images: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/'
 };
