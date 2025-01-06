bf9787b965cca453af28c8171588ee4b5cb900b0
edsonstudio <edson.costa.developer@gmail.com>
2025-01-06 19:36:12 -0300

    fix: ajustadas as urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrlV1: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/api/v1',
-  apiUrlV2: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/api/v2',
-  apiChatUrl: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2',
-  apiAuthUrlV2: 'https://dragonstore-auth-f5cuf5ewbvh7dygq.brazilsouth-01.azurewebsites.net/api/v2/identity',
-  apiShoppingUrlV1: 'https://dragonstore-bff-cjcne7ahgwb3fjg6.brazilsouth-01.azurewebsites.net//api/v1/compras',
+  apiUrlV1: 'https://catalog-dragonstore-hjedfrhugwhpdsdd.northeurope-01.azurewebsites.net/api/v1',
+  apiUrlV2: 'https://catalog-dragonstore-hjedfrhugwhpdsdd.northeurope-01.azurewebsites.net/api/v2',
+  apiChatUrl: 'https://auth-dragonstore-h6fdd6gde2gedndy.northeurope-01.azurewebsites.net/api/v2',
+  apiAuthUrlV2: 'https://auth-dragonstore-h6fdd6gde2gedndy.northeurope-01.azurewebsites.net/api/v2/identity',
+  apiShoppingUrlV1: 'https://dragonstore-bff-cjcne7ahgwb3fjg6.brazilsouth-01.azurewebsites.net/api/v1/compras',
   apiClientUrl: 'https://dragonstore-client-g4bydzetb8h4b7dt.brazilsouth-01.azurewebsites.net/api/v1/client/address',
-  images: 'https://dragonstore-products-emhpcjg0hreea0hc.brazilsouth-01.azurewebsites.net/'
+  images: 'https://catalog-dragonstore-hjedfrhugwhpdsdd.northeurope-01.azurewebsites.net/'
 };
